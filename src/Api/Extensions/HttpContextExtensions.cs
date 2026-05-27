00ff3dad0acae97625828bfcefe5a65498019307
DavidChen617 <a880617c@gmail.com>
2026-05-27 15:11:19 +0800

    refactor(api): HttpContextExtensions 改用 C# 14 extension 語法

--- src/Api/Extensions/HttpContextExtensions.cs
+++ src/Api/Extensions/HttpContextExtensions.cs
@@ -4,16 +4,19 @@ namespace Api.Extensions;
 
 public static class HttpContextExtensions
 {
-    public static bool TryGetUserId(this HttpContext ctx, out UserId userId)
+    extension(HttpContext ctx)
     {
-        if (ctx.Request.Headers.TryGetValue("X-User-Id", out var value) &&
-            Guid.TryParse(value, out var guid))
+        public bool TryGetUserId(out UserId userId)
         {
-            userId = new UserId(guid);
-            return true;
-        }
+            if (ctx.Request.Headers.TryGetValue("X-User-Id", out var value) &&
+                Guid.TryParse(value, out var guid))
+            {
+                userId = new UserId(guid);
+                return true;
+            }
 
-        userId = default!;
-        return false;
+            userId = null!;
+            return false;
+        }
     }
 }
